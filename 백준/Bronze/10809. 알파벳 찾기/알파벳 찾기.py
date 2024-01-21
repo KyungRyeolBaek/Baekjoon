11e7a592a4005aa26511f2f8a74e33ba3e63e812
KyungRyeol <foreversanta@naver.com>
2024-01-21 23:08:31 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,1 +1,11 @@
-print(*map(input().find,map(chr,range(97,123))))
+import sys
+
+
+input = sys.stdin.readline
+answer = ['-1' for _ in range(26)]
+for idx, string in enumerate(input().strip()):
+    pos = ord(string) - 97
+    if answer[pos] == '-1':
+        answer[pos] = str(idx)
+
+print(' '.join(answer))
