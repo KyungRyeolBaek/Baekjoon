d09ff0e427b42276e8965e761ab4197b1cf0d1ca
KyungRyeol <foreversanta@naver.com>
2024-03-17 21:05:50 +0900

    [Bronze II] Title: 분해합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -4,7 +4,8 @@ import sys
 input = sys.stdin.readline
 N = int(input().strip())
 min_value = N
-for c in range(N, 1, -1):
+init_sum = sum(map(int, list(str(N))))
+for c in range(max(N - 54, 1), max(N - init_sum + 1, N)):
     if N == c + sum(map(int, list(str(c)))) and min_value > c:
         min_value = c
 
