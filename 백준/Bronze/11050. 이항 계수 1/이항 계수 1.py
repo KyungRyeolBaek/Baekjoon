14cbf1abe5490d88bb32ba6fc899c055d62a71c3
KyungRyeol <foreversanta@naver.com>
2024-06-16 17:00:07 +0900

    [Bronze I] Title: 이항 계수 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def bino_coef(n, k):
+    if k == 0 or n == k:
+        return 1
+    return bino_coef(n-1, k) + bino_coef(n-1, k-1)
+
+
+input = sys.stdin.readline
+print(bino_coef(*map(int, input().strip().split())))
