83e080063e1930cfb671e284bd10ed9e846bec94
KyungRyeol <foreversanta@naver.com>
2024-08-08 00:28:54 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 440 ms, Memory: 152988 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -2,9 +2,15 @@ import sys
 
 
 input = sys.stdin.readline
-S = input().strip()
+S = input().rstrip()
+dict_S = {chr(s): [0] * (len(S) + 1) for s in range(97, 123)}
+for i in range(len(S)):
+    for char in dict_S:
+        dict_S[char][i + 1] = dict_S[char][i]
+    dict_S[S[i]][i + 1] += 1
+
 N = int(input())
 for _ in range(N):
-    a, l, r = input().strip().split()
+    a, l, r = input().rstrip().split()
     l, r = map(int, [l, r])
-    print(S[l:r + 1].count(a))
+    print(dict_S[a][r + 1] - dict_S[a][l])
